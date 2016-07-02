42efbb8f160322bdf65a002af71e7944291d5829
hgs <hgs.dev@gmail.com>
2016-07-03 01:22:40 +0900

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var run = require('gulp-run');
+var rename = require("gulp-rename");
+
+gulp.task('create_svg', function(){
+  return run('./cli.js').exec()
+    .pipe(rename('test.svg'))
+    .pipe(gulp.dest('tmp/'))
+    .pipe(rename('test.html'))
+    .pipe(gulp.dest('tmp/'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./cli.js', ['create_svg']);
+});
