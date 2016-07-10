dba7f7dc7e83947ec31926bf7c522ee8e023b8a6
hgs <hgs.dev@gmail.com>
2016-07-10 21:28:00 +0900

    Add check feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
 var rename = require("gulp-rename");
+var gulpClangFormat = require("gulp-clang-format");
+var gulpEslint = require("gulp-eslint");
 
 gulp.task('create_svg', function(){
   return run('./cli.js ./sample/tasks.yaml').exec()
@@ -12,4 +14,30 @@ gulp.task('create_svg', function(){
 
 gulp.task('watch', function(){
   gulp.watch(['./cli.js', './src/**'], ['create_svg']);
+});
+
+gulp.task('check', ['lint-javascript', 'check-javascript-format']);
+
+/**
+ * Lints all projects JavaScript files using ESLint. This includes frontend sour
+ce code, as well as,
+ * build scripts.
+ */
+gulp.task('lint-javascript', function() {
+  return gulp
+    .src('src/**/*.js')
+    // Attach lint output to the eslint property of the file.
+    .pipe(gulpEslint())
+    // Output the lint results to the console.
+    .pipe(gulpEslint.format())
+    // Exit with an error code (1) on a lint error.
+    .pipe(gulpEslint.failOnError());
+});
+
+/**
+ * Checks whether project's JavaScript files are formatted according to clang-format style.
+ */
+gulp.task('check-javascript-format', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(gulpClangFormat.checkFormat('file', undefined, {verbos: true, fail: true}));
 });
