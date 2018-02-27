6273f6b7b888f6b4d05e3f764d89812573e30c2e
ihgs <hgs.dev@gmail.com>
2018-02-27 22:04:15 +0900

    Fix cli.js

--- cli.js
+++ cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 'use strict';
-var cmd = require('commander')
-var fs = require('fs')
-var path = require('path')
-var svg2png = require('svg2png')
-var gant = require('./src/js/gantt.js')
-var yaml = require('./src/js/yaml_parser')
+const cmd = require('commander');
+const fs = require('fs');
+const path = require('path');
+const svg2png = require('svg2png');
+const gant = require('./src/js/gantt.js');
+const yaml = require('./src/js/yaml_parser');
 
-var program = cmd.version('0.1.0')
+const program = cmd.version('0.1.0')
   .command('yaml2gantt')
   .usage('[options] <file>')
   .option('-c, --config <config>', 'Set config path. default to ./config.yaml')
@@ -16,9 +16,9 @@ var program = cmd.version('0.1.0')
   .option('--compare <compare_file>', 'Set file which you want to compare')
   .option('--stdout', 'Output to stdout [Default: output to a file.]')
   .arguments('yaml_path')
-  .parse(process.argv)
+  .parse(process.argv);
 
-var config = {
+let config = {
   "canvas": {
     "width": 1000,
     "height": 300
@@ -42,33 +42,36 @@ var config = {
       "11月", "12月"
     ]
   }
-}
+};
 
 if (program.config) {
-  var config_file = program.config
-  config = yaml.config(config_file)
+  const config_file = program.config;
+  config = yaml.config(config_file);
 } else {
-  var config_file = './config.yaml'
+  const config_file = './config.yaml';
   try{
-    var stat = fs.statSync(config_file);
+    const stat = fs.statSync(config_file);
     if (stat != undefined && stat.isFile()){
-      config = yaml.config(config_file)
+      config = yaml.config(config_file);
     }
-  }catch(e){}
+  }catch(e){
+    //no operation
+  }
 }
 
-var filepath = program.args[0]
+const filepath = program.args[0];
 if (filepath == undefined) {
   program.help();
 }
 
-var compare_file = program.compare
+let data;
+const compare_file = program.compare;
 if (compare_file == undefined){
-  var data = yaml.parse(filepath)
+  data = yaml.parse(filepath);
 }else {
-  var data = yaml.parse(filepath)
-  var compare_data = yaml.parse(compare_file, data["range"]["start"], data["range"]["end"])
-  data["resources"]["compared_tasks"] = []
+  data = yaml.parse(filepath);
+  const compare_data = yaml.parse(compare_file, data["range"]["start"], data["range"]["end"]);
+  data["resources"]["compared_tasks"] = [];
   for (let key in data["resources"]["tasks"]) {
     for( let cKey in compare_data["resources"]["tasks"]){
       if (data["resources"]["tasks"][key]["id"] == compare_data["resources"]["tasks"][cKey]["id"]){
@@ -81,22 +84,24 @@ if (compare_file == undefined){
 gant.init(data.range, config);
 gant.update(data.resources);
 
-var output_file = program.output;
-var format = program.format || 'html';
-var stdout = program.stdout;
+let output_file = program.output;
+const format = program.format || 'html';
+const stdout = program.stdout;
 
-var out;
+let out;
 if ( format == 'svg' || format == 'png'){
   out = '<?xml version="1.0" encoding="utf-8"?>' + document.body.innerHTML;
 } else if (format == 'html' ) {
-  out = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>'+ document.body.innerHTML + '</body></html>'
+  out = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>'+ document.body.innerHTML + '</body></html>';
 } else {
-  console.error('Format:' + format + ' is not supported.')
+  console.error('Format:' + format + ' is not supported.');
+  // eslint-disable-next-line no-process-exit
   process.exit(1);
 }
 if ( stdout ){
   if ( format == 'png' ){
     console.error('stdout option is not used when format is png.');
+    // eslint-disable-next-line no-process-exit
     process.exit(1);
   }
   console.log(out);
